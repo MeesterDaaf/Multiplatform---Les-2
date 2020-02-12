2327bd21d2628b18c08b7508b03483ff2bab6395
David van Oudheusden <d.vanoudheusden@rocva.nl>
2020-02-12 16:55:52 +0100

    opdrachten gemaakt

---
+++ opdracht-strings.py
@@ -0,0 +1,15 @@
+
+# Strings
+
+### opdracht - print Hello World ###
+
+# 1. 'Vul' een variabele met de string 'Hello World!'
+# 2. print deze op het scherm
+
+### opdracht - print Mijn naam is.... ###
+
+# 1. 'Vul' een variabele met de input van de gebruiker 
+# 2. print deze op het scherm
+
+
+input_gebruiker = 
